1168fe0d373247320d78da6575bedcac74ba32aa
neistow <glebchernigin236@gmail.com>
2022-01-13 23:16:20 +0200

    lab-5 done

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,82 +1,52 @@
--- display vehicle types and associated routes
--- where vehicle total seat count > 100
-select vt.name, r.id
+-- for each vehicle type display max,min,avg seat count
+
+select *
+from vehicle_types vt
+         cross apply (
+    select max(v.seats) as max_seats,
+           min(v.seats) as min_seats,
+           avg(v.seats) as avg_seats
+    from vehicles v
+    where v.type_id = vt.id
+) as x;
+
+go
+
+-- sum of seats in vehicle types
+select v.id,
+       sum(seats) over(partition by v.type_id) as s
+from vehicles v
+
+go
+
+-- partition by assigned route count + adding idx to result sequence 
+select v.id as vehicle_id,
+       count(v.id) as assigned_routes,
+       row_number() over(partition by count(v.id) order by count(v.id)) as seq_num
+from vehicles v
+         join vehicle_routes vr on v.id = vr.vehicle_id
+group by v.id
+
+go
+
+-- average seats in veh type
+select distinct v.type_id,
+                avg(v.seats)
+                    over (partition by v.type_id order by v.type_id desc) as avg_seats_in_type
+from vehicles v
+         left outer join vehicle_types vt on vt.id = v.type_id
+
+go
+
+-- flat average seats
+select average, [1] as veh_type_1, [2] as veh_type_2, [3] as veh_type_3
+from (select 'average seats' as average, type_id, seats from vehicles) v
+         pivot (avg(seats) for type_id in ([1], [2], [3])) pvt
+
+go
+
+-- rollup
+select vt.name, count(*) as total_vehicles_of_type, sum(v.seats) total_seats
 from vehicle_types vt
          left join vehicles v on vt.id = v.type_id
-         left join vehicle_routes vr on v.id = vr.vehicle_id
-         left join routes r on r.id = vr.route_id
-group by vt.name, r.id
-having sum(v.seats) > 100
-
-go
-
--- display assigned routes count for vehicle
-select identifier, assigned_routes
-from (
-         select v.id, v.identifier, sum(vr.route_id) as assigned_routes
-         from vehicles v
-                  join vehicle_routes vr on v.id = vr.vehicle_id
-         group by v.id, v.identifier
-     ) assigned_veh_routes
-where assigned_routes > 1
-
-go
-
--- display count of vehicles by type
-select vt.name,
-       (select count(*) from vehicles where type_id = vt.id) as veh_count
-from vehicle_types vt
-
-go
-
--- Variable demo
-declare @free_transport table(transport_id int)
-
-insert into @free_transport(transport_id)
-select v.id as transport_id
-from vehicles v
-         left join vehicle_routes vr on v.id = vr.vehicle_id
-where route_id is null
-
-select * from @free_transport
-
-go
-
--- Temp table demo
-drop table if exists #Temp
-create table #Temp(veh_type varchar(64), max_seats int);
-
-insert into #Temp
-select vt.name, max(v.seats)
-from vehicles v
-         left join vehicle_types vt on vt.id = v.type_id
-group by vt.name
-
-select * from #Temp
-
-go
-
--- CTE demo
-with veh_type_with_veh_count as (
-    select vt.id, count(*) as vehicle_count
-    from vehicle_types vt
-             left join vehicles v on vt.id = v.type_id
-    group by vt.id
-)
-select avg(vehicle_count)
-from veh_type_with_veh_count
-
-go
-
--- view demo
-create view trans_port_type_veh_units
-as
-select vt.name,
-       (select count(*) from vehicles where type_id = vt.id) as veh_count
-from vehicle_types vt
-
-select vt.id, vt.name, t.veh_count
-from trans_port_type_veh_units t
-         left join vehicle_types vt on vt.name = t.name
-
-go
+group by vt.name with rollup 
