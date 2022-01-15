54de9d230c556a4aa964c9426acfc5d6e7f92895
neistow <glebchernigin236@gmail.com>
2022-01-16 00:22:31 +0200

    Revert "lab-11 done"
    This reverts commit a5ab60218d135c9acbcb4ed3b3eb7f91257b2fd3.

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,37 +1,99 @@
-create trigger veh_insert
-    on vehicles
-    after insert
-    as
-    declare cur cursor local for 
-        select type_id, count(*) as amount from inserted group by type_id
-    declare @type int, @amount int;
+begin transaction
+declare @result int
+exec sp_populate_vehicles 1, null, @processed_rows = @result output
+commit transaction
 
-    open cur
-    fetch next from cur into @type, @amount
-    while @@fetch_status = 0
-        begin
-            update vehicle_types set vehicle_count = vehicle_count + @amount where id = @type
-            fetch next from cur into @type, @amount
-        end
-    close cur
-    deallocate cur
+begin transaction
+update vehicles
+set seats = seats + 1
+if exists(select 1
+          from vehicles
+          where seats = 99)
+    rollback transaction
+else
+    commit transaction
 
------
 
-create trigger veh_delete
-    on vehicles
-    after delete 
-    as
-    declare cur cursor local for
-        select type_id, count(*) as amount from deleted group by type_id
-    declare @type int, @amount int;
+begin transaction
+select *
+from vehicles
+where seats > 25
+select *
+from routes
+where number like '%oute %'
+commit transaction
 
-    open cur
-    fetch next from cur into @type, @amount
-    while @@fetch_status = 0
-        begin
-            update vehicle_types set vehicle_count = vehicle_count - @amount where id = @type
-            fetch next from cur into @type, @amount
-        end
-    close cur
-    deallocate cur
+
+--- try-catch
+begin try
+    begin transaction
+        update vehicles set seats = seats + 10000
+    commit transaction
+end try
+begin catch
+    rollback transaction
+end catch
+
+--- nested
+begin transaction one
+select *
+from routes
+update routes
+set number = concat('_', number)
+select *
+from routes
+begin transaction two
+update routes
+set number = concat('__', number)
+select *
+from routes
+rollback transaction
+select *
+from routes
+
+--- savepoints
+begin transaction one
+update routes
+set number = concat('_', number)
+select *
+from routes
+
+save transaction one
+
+update routes
+set number = concat('__', number)
+select *
+from routes
+
+rollback transaction one
+
+select *
+from routes
+
+--- deadlock example
+
+set transaction isolation level serializable
+begin transaction
+update routes set number = concat('route-', id)
+select * from vehicles;
+commit tran
+
+--- to fix lower isolation
+set transaction isolation level serializable
+begin transaction
+update vehicles set identifier = identifier
+select * from routes
+commit transaction
+
+--- read uncommitted deadlock
+set transaction isolation level read uncommitted
+begin transaction
+update vehicles set identifier = identifier
+update routes set number = number
+commit transaction
+
+set transaction isolation level read uncommitted
+begin transaction
+update routes set number = number
+update vehicles set identifier = identifier
+commit transaction
