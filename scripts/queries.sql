e5519e9a8a54f3e8dee46a904b68441960d786d7
neistow <glebchernigin236@gmail.com>
2022-01-15 13:28:43 +0200

    lab-6 done

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,52 +1,85 @@
--- for each vehicle type display max,min,avg seat count
+SET SHOWPLAN_TEXT On
+go
+SET SHOWPLAN_ALL On
+go
+SET SHOWPLAN_XML On
+go
+SET STATISTICS PROFILE On
+go
+SET STATISTICS XML On
+
+create table tickets
+(
+    id             int          not null,
+    name           varchar(255) not null,
+    price          money        not null,
+    expirationDate datetime2    not null,
+);
+
+-- |--Table Scan(OBJECT:([transport].[dbo].[Tickets]))
+select *
+from tickets
+
+drop table tickets;
+create table tickets
+(
+    id             int          primary key,
+    name           varchar(255) not null,
+    price          money        not null,
+    expirationDate datetime2    not null,
+);
+
+-- |--Clustered Index Scan(OBJECT:([transport].[dbo].[Tickets].[PK__Tickets__3213E83F6FDD327D]))
+select *
+from tickets
+
+
+--   |--Clustered Index Seek(OBJECT:([transport].[dbo].[Tickets].[PK__Tickets__3213E83F6FDD327D]),
+--   SEEK:([transport].[dbo].[Tickets].[id]=CONVERT_IMPLICIT(int,[@1],0)) ORDERED FORWARD)
+select *
+from tickets
+where id = 10
+
+create nonclustered index idx_exp_dte on tickets(expirationDate)
+
+-- Clustered Index Scan(OBJECT:([transport].[dbo].[tickets].[PK__tickets__3213E83FD37AF2AF]),
+-- WHERE:([transport].[dbo].[tickets].[expirationDate]=getdate()))
+select *
+from tickets
+where expirationDate = GETDATE()
+
+-- |--Nested Loops(Inner Join, OUTER REFERENCES:([transport].[dbo].[tickets].[id]))"
+--    |--Index Scan(OBJECT:([transport].[dbo].[tickets].[idx_exp_dte]))
+--    |--Clustered Index Seek(OBJECT:([transport].[dbo].[tickets].[PK__tickets__3213E83FD37AF2AF]),
+--       SEEK:([transport].[dbo].[tickets].[id]=[transport].[dbo].[tickets].[id]) 
+--       LOOKUP ORDERED FORWARD)"
 
 select *
-from vehicle_types vt
-         cross apply (
-    select max(v.seats) as max_seats,
-           min(v.seats) as min_seats,
-           avg(v.seats) as avg_seats
-    from vehicles v
-    where v.type_id = vt.id
-) as x;
+from tickets
+         with (index (idx_exp_dte))
 
+create nonclustered index idx_price on tickets(name, price)
+
+-- |--Clustered Index Scan(OBJECT:([transport].[dbo].[tickets].[PK__tickets__3213E83FD37AF2AF]),
+-- WHERE:([transport].[dbo].[tickets].[price]=[@1]))
+select *
+from tickets
+where price = 25.5
+
+--   |--Index Scan(OBJECT:([transport].[dbo].[tickets].[idx_price]),
+--   WHERE:([transport].[dbo].[tickets].[price]=[@1]))
+select name, price
+from tickets
+where price = 25.5
+
+SET SHOWPLAN_TEXT OFF
 go
-
--- sum of seats in vehicle types
-select v.id,
-       sum(seats) over(partition by v.type_id) as s
-from vehicles v
-
+SET SHOWPLAN_ALL OFF
 go
-
--- partition by assigned route count + adding idx to result sequence 
-select v.id as vehicle_id,
-       count(v.id) as assigned_routes,
-       row_number() over(partition by count(v.id) order by count(v.id)) as seq_num
-from vehicles v
-         join vehicle_routes vr on v.id = vr.vehicle_id
-group by v.id
-
+SET SHOWPLAN_XML OFF
 go
-
--- average seats in veh type
-select distinct v.type_id,
-                avg(v.seats)
-                    over (partition by v.type_id order by v.type_id desc) as avg_seats_in_type
-from vehicles v
-         left outer join vehicle_types vt on vt.id = v.type_id
-
+SET STATISTICS PROFILE OFF
 go
+SET STATISTICS XML OFF
 
--- flat average seats
-select average, [1] as veh_type_1, [2] as veh_type_2, [3] as veh_type_3
-from (select 'average seats' as average, type_id, seats from vehicles) v
-         pivot (avg(seats) for type_id in ([1], [2], [3])) pvt
-
-go
-
--- rollup
-select vt.name, count(*) as total_vehicles_of_type, sum(v.seats) total_seats
-from vehicle_types vt
-         left join vehicles v on vt.id = v.type_id
-group by vt.name with rollup 
+drop table tickets;
